de27d6d39818d1985dae73947648fc76b0f49f44
Petru Marginean <petru.marginean@gmail.com>
2019-07-14 23:27:08 -0400

    misc

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#define LOG std::cerr << __FUNCTION__ << std::endl
+
+int main(int argc, char* argv[])
+{
+    assert(argc == 2);
+    
+    return 0;
+}
