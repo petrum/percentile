100ce01bdf0d68e1b21f2bcb35c8779f5b4c4b3d
Petru Marginean <petru.marginean@gmail.com>
2019-07-14 23:42:10 -0400

    misc

--- main.cpp
+++ main.cpp
@@ -4,15 +4,35 @@
 
 #define LOG std::cerr << __FUNCTION__ << std::endl
 
+struct Percentile
+{
+    Percentile(double d) : percentile(d) {}
+    void add(double);
+    double get() const;
+private:
+    double percentile;
+};
+
+void Percentile::add(double)
+{
+}
+
+double Percentile::get() const
+{
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     assert(argc == 2);
-    double percentile = std::stod(argv[1]);
-    std::cerr << "Percentile = " << percentile << "%\n";
+    double d = std::stod(argv[1]);
+    std::cerr << "- percentile = " << d << "%\n";
+    Percentile p(d);
     double n;
     while (std::cin >> n)
     {
-        std::cout << n << std::endl;
+        p.add(n);
+        std::cout << p.get() << std::endl;
     }
     return 0;
 }
