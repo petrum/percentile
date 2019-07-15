bcb7f2d9ec6cbff5f2d5acba1d9855e3811daa73
Petru Marginean <petru.marginean@gmail.com>
2019-07-14 23:36:41 -0400

    misc

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@
 int main(int argc, char* argv[])
 {
     assert(argc == 2);
-    
+    double percentile = std::stod(argv[1]);
+    std::cerr << "Percentile = " << percentile << "%\n";
+    double n;
+    while (std::cin >> n)
+    {
+        std::cout << n << std::endl;
+    }
     return 0;
 }
